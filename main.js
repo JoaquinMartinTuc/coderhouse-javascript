115f66c11b64d6cd060fcc6d485e3b50919caff5
Joaquin <joaquin.martin2003@gmail.com>
2023-10-18 03:09:19 -0300

    segunda pre entrega, cambie la idea de la página y agregue lo visto en clase

--- main.js
+++ main.js
@@ -1,50 +1,130 @@
-let nombre = prompt("Bienvenido a la web de Replat Escapes, por favor ingresar su nombre debajo.");
-if (nombre === ""){
-    alert ("No ingresaste tu nombre");
-}else {
-    alert (`Hola ${nombre}, a continuacion vas a poder elegir el accesorio para tu auto`);
+let nombre;
+
+while (!nombre) {
+    nombre = prompt("Bienvenido a la web de Cens Sport, por favor ingrese su nombre debajo.");
 }
 
-function recomprar() {
-    pregunta = prompt ("Desea realizar otra compra? SI/NO");
+alert(`Hola ${nombre}, a continuación podrás elegir tu prenda de ropa.`);
+
+
+const productos = [
+    { prenda: "remera Oversize", color: ["Negro", "Blanco", "Rojo", "Morado"], precio: 4500 },
+    { prenda: "remera Clásica", color: ["Blanco", "Negro", "Azul"], precio: 3500 },
+    { prenda: "remera Estampada", color: ["Azul", "Negro"], precio: 5000 },
+    { prenda: "jean Recto", color: ["Azul"], precio: 7000 },
+    { prenda: "jean Oversize", color: ["Blanco", "Azul"], precio: 8000 },
+    { prenda: "jean Chupin", color: ["Azul", "Negro"], precio: 6000 },
+    { prenda: "medias 3/4", color: ["Blanco", "Negro"], precio: 1500 },
+    { prenda: "medias soquete", color: ["Blanco", "Negro"], precio: 1000 }
+];
+
+
+// Para agregar un producto, generar un nuevo elemento 
+
+const nuevoProducto = { prenda: "gorra", color: ["Negro"], precio: 4000 };
+productos.push(nuevoProducto);
+
+//
+
+let eleccionTipo;
+
+while (true) {
+    eleccionTipo = prompt("¿Desea buscar por prenda o por color?").toLowerCase();
+
+    if (eleccionTipo === "prenda" || eleccionTipo === "color") {
+        break;
+    } else {
+        alert("Por favor, elija 'prenda' o 'color'");
+    }
 }
 
-let total = 0
+let eleccion;
 
-let pregunta = "si";
-while (pregunta === "si") {
-let producto = prompt("Qué accesorio desea comprar?\n-Amortiguador\n-Enganche\n-Multiple").toLowerCase();
-if(producto === ""){
-    alert ("No ingresaste ningún producto");
-}else if(producto === "amortiguador"){
-    alert ("Precio de cada Amortiguador: $100");
-    recomprar();
-    total = total + 100;
-
-}else if(producto === "enganche"){
-    alert ("Precio de cada Enganche: $80");
-    recomprar();
-    total = total + 80;
-    
-
-}else if(producto === "multiple"){
-    alert("Precio de cada Multiple = $120");
-    recomprar();
-    total = total + 120;
-    
-
-}else {
-    alert ("El accesorio no esta disponible o escribiste el nombre incorrectamente")
-}
+while (!eleccion) {
+    eleccion = prompt("Ingrese el nombre de la prenda o el color que desea:\nPrendas: Remera, Jean, Medias, Gorra\nColor: Blanco, Negro, Azul, Rojo, Morado").toLowerCase();
 }
 
-alert (`El precio total de su compra es de ${total}`)
+if (eleccionTipo === "prenda") {
+    const resultado = productos.filter((producto) => {
+        return producto.prenda.toLowerCase().includes(eleccion) || producto.color.some(color => color.toLowerCase().includes(eleccion));
+    });
+
+    if (resultado.length > 0) {
+        let mensaje = "Información de las prendas seleccionadas:\n\n";
+        resultado.forEach((producto) => {
+            mensaje += `Prenda: ${producto.prenda}\nColores: ${producto.color.join(', ')}\nPrecio: $${producto.precio}\n\n`;
+        });
+        alert(mensaje);
+    } else {
+        alert(`No se encontraron productos con ese nombre o color.`);
+    }
+} else if (eleccionTipo === "color") {
+    const resultado = productos.filter((producto) => {
+        return producto.color.some(color => color.toLowerCase().includes(eleccion));
+    });
+
+    if (resultado.length > 0) {
+        let mensaje = "Información de las prendas con el color seleccionado:\n\n";
+        resultado.forEach((producto) => {
+            mensaje += `Prenda: ${producto.prenda}\nColores: ${producto.color.join(', ')}\nPrecio: $${producto.precio}\n\n`;
+        });
+        alert(mensaje);
+    } else {
+        alert(`No se encontraron productos con ese color.`);
+    }
+}
+
+const carrito = [];
+
+function mostrarProductos() {
+    let mensaje = "Productos disponibles:\n";
+    productos.forEach((producto, index) => {
+        mensaje += `${index + 1}. ${producto.prenda} - Colores: ${producto.color.join(', ')} - Precio: $${producto.precio}\n`;
+    });
+    return mensaje;
+}
+
+function agregarAlCarrito(productoIndex, cantidad) {
+    const producto = productos[productoIndex];
+    carrito.push({ producto, cantidad });
+    return `${cantidad} ${producto.prenda} (${producto.color.join(', ')}) agregadas al carrito.`;
+}
+
+function mostrarCarrito() {
+    let mensaje = "Contenido del carrito:\n";
+    let total = 0;
+    carrito.forEach((item) => {
+        const { producto, cantidad } = item;
+        mensaje += `${cantidad} ${producto.prenda} (${producto.color.join(', ')}) - Precio: $${producto.precio * cantidad}\n`;
+        total += producto.precio * cantidad;
+    });
+    mensaje += `Total de la compra: $${total}`;
+    return mensaje;
+}
+
+while (true) {
+    const opcion = prompt(
+        mostrarProductos() +
+        "\nIngrese el número del producto que desea agregar al carrito (o 'terminar' para finalizar la compra):"
+    );
+
+    if (opcion.toLowerCase() === "terminar") {
+        alert(mostrarCarrito());
+        break;
+    }
+
+    const productoIndex = parseInt(opcion) - 1;
+    if (productoIndex >= 0 && productoIndex < productos.length) {
+        const cantidad = parseInt(prompt(`¿Cuántos ${productos[productoIndex].prenda} (${productos[productoIndex].color.join(', ')}) desea agregar al carrito?`));
+        if (!isNaN(cantidad) && cantidad > 0) {
+            alert(agregarAlCarrito(productoIndex, cantidad));
+            alert(mostrarCarrito());
+        } else {
+            alert("Cantidad no válida. Intente de nuevo.");
+        }
+    } else {
+        alert("Número de producto no válido. Intente de nuevo.");
+    }
+}
 
 alert ("Muchas gracias por su compra, lo esperamos de vuelta")
-
-
-
-
-
-
-
